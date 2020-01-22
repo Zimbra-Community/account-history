eadde60aa2d415b7db06b205a6d2b46bbeb6ee29
Barry de Graaff <info@barrydegraaff.tk>
2020-01-22 13:37:54 +0100

    fix:https://github.com/Zimbra-Community/account-history/issues/22

--- bin/geoip2-zimbra-update.sh
+++ bin/geoip2-zimbra-update.sh
@@ -3,6 +3,9 @@ echo "Installing/updating GeoIP2"
 rm -Rf /opt/GeoIP2-Zimbra
 mkdir /opt/GeoIP2-Zimbra
 cd /opt/GeoIP2-Zimbra
-wget https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz
+wget https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=ShaZphSuBJYC5vgf&suffix=tar.gz -O GeoLite2-City.tar.gz
 tar --strip-components 1 -xvf GeoLite2-City.tar.gz
 rm -f /opt/GeoIP2-Zimbra/GeoLite2-City.tar.gz
+
+
+
